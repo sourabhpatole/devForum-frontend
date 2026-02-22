a4e45b30a9dca77215c050d6ec88cdcffbf7c574
Sourabh <sourabhpatole@outlook.com>
2026-02-22 18:23:34 +0530

    finished send/ignore funtionality

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,24 +1,55 @@
+import axios from "axios";
 import React from "react";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { removeUserFromFeed } from "../utils/feedSlice";
 
 const UserCard = ({ user }) => {
-  //   console.log(user);
-  const { photoUrl, firstName, lastName, age, gender, about } = user;
-
+  const dispatch = useDispatch();
+  console.log(user);
+  const { _id, photoUrl, firstName, lastName, age, gender, about } = user;
+  // console.log(user);
+  const handleSubmit = async (status, userId) => {
+    try {
+      const res = await axios.post(
+        `${BASE_URL}/request/send/${status}/${userId}`,
+        {},
+        {
+          withCredentials: true,
+        },
+      );
+      dispatch(removeUserFromFeed(userId));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  if (user == null)
+    return <h1 className="flex justify-center ">No User found</h1>;
   return (
-    <div className="card bg-base-300 w-96 shadow-sm">
-      <figure>
-        <img src={photoUrl} alt="Shoes" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{firstName + " " + lastName}</h2>
-        {age && gender && <p>{age + " " + gender}</p>}
-        <p>{about}</p>
-        <div className="card-actions justify-center my-4">
-          <button className="btn btn-primary">Ignore</button>
-          <button className="btn btn-secondary">Interested</button>
+    <>
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <figure>{photoUrl && <img src={photoUrl} alt="Shoes" />}</figure>
+        <div className="card-body">
+          <h2 className="card-title">{firstName + " " + lastName}</h2>
+          {age && gender && <p>{age + " " + gender}</p>}
+          <p>{about}</p>
+          <div className="card-actions justify-center my-4">
+            <button
+              className="btn btn-primary"
+              onClick={() => handleSubmit("ignored", _id)}
+            >
+              Ignore
+            </button>
+            <button
+              className="btn btn-secondary"
+              onClick={() => handleSubmit("interested", _id)}
+            >
+              Interested
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
